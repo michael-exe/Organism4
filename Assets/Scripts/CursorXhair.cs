70843c3fb4bcc04c8128a143d10a5dd198dff461
Michelangelo <michelangelo.dev.work@gmail.com>
2022-01-22 12:43:55 +0100

    mp_itsolutions fix2

--- Assets/Scripts/CursorXhair.cs
+++ Assets/Scripts/CursorXhair.cs
@@ -10,6 +10,7 @@ public class CursorXhair : MonoBehaviour
     //public SpriteRenderer spriteRenderer;
     public Sprite newSprite;
     public Sprite originalSprite;
+    bool hasCollided;
     List<SpriteRenderer> collidingObjects = new List<SpriteRenderer>();
     private LayerMask mask;
 
@@ -37,6 +38,13 @@ public class CursorXhair : MonoBehaviour
             .Where(_ => _ != null)
             .Where(_ => _.CompareTag("Int_Molecule"));
 
+        var hitObject = Physics2D.Linecast(mousePos, playerPos, mask);
+
+        if (hitObject.collider != null)
+        {
+            hitObject.collider.GetComponent<SpriteRenderer>();
+        }
+        
         //iterate enumerable 
         foreach (var spriteRenderer in hitObjects)
         {
@@ -44,19 +52,30 @@ public class CursorXhair : MonoBehaviour
             if (!collidingObjects.Contains(spriteRenderer))
             {
                 collidingObjects.Add(spriteRenderer);
-                spriteRenderer.sprite = newSprite;
             }
         }
 
-        //this is a sublist made with Linq new list method
-        var notLongertHitObjects = collidingObjects.Except(hitObjects).ToList();
-        //for needs [i]
+        //this is a sublist made with Linq "new list" method
+        var notLongetHitObjects = collidingObjects.Except(hitObjects).ToList();
+        //"for" needs [i]
         //for iterates a specific number of times
-        //everytime loop add one to check all elements
-        for (var i = 0; i < notLongertHitObjects.Count; i++)
+        //everytime it loops add one to check all elements
+        for (var i = 0; i < notLongetHitObjects.Count; i++)
         {
-            notLongertHitObjects[i].sprite = originalSprite;
-            collidingObjects.Remove(notLongertHitObjects[i]);
+            notLongetHitObjects[i].sprite = originalSprite;
+            collidingObjects.Remove(notLongetHitObjects[i]);
+        }
+
+        if (collidingObjects.Count == 0)
+        {
+            return;
+        }
+
+        var closestObject = collidingObjects.OrderBy(_ => (_.transform.position - (Vector3)mousePos).sqrMagnitude).First();
+        closestObject.sprite = newSprite;
+        foreach (var spriteRenderer in collidingObjects.Where(_ => _ != closestObject))
+        {
+            spriteRenderer.sprite = originalSprite;
         }
     }
 }
