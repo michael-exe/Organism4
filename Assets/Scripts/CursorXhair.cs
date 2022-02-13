4668a2ef14338cb82b10246707803e1c5600a379
Michelangelo <michelangelo.dev.work@gmail.com>
2022-02-14 00:32:47 +0100

    xhair rework

--- Assets/Scripts/CursorXhair.cs
+++ Assets/Scripts/CursorXhair.cs
@@ -8,76 +8,44 @@ public class CursorXhair : MonoBehaviour
 {
     public Player Player;
     public AttachmentController Molecule;
+    //public SpriteRenderer spriteRenderer;
     public Sprite newSprite;
     public Sprite originalSprite;
-    //bool hasCollided;
     List<SpriteRenderer> collidingObjects = new List<SpriteRenderer>();
-    private LayerMask mask;
-    
-
-
-    private void Awake()
-    {
-        mask = ~(1 << LayerMask.NameToLayer("Player"));
-    }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 playerPos = Player.transform.position;
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 debugDirection = mousePos - playerPos;
+        Vector2 direction = mousePos - playerPos;
 
         transform.position = mousePos;
 
-        Debug.DrawRay(playerPos, debugDirection);
-        //longer linq definition instead of multiple && if conditions/list
-        //I declare and define an enumerable variable
-        //_ => _ means to pass any viable parameter to consider it
-        //Similar to Linecast except that all Colliders are reported
-        var hitObjects = Physics2D.LinecastAll(mousePos, playerPos, mask)
-            .Select(_ => _.collider.GetComponent<SpriteRenderer>())
-            .Where(_ => _ != null)
-            .Where(_ => _.CompareTag("Int_Molecule"));
+        Debug.DrawRay(playerPos, direction);
+        //Linecast already stops at first collision unlike LinecastAll
+        RaycastHit2D hitObjects = Physics2D.Linecast(mousePos, playerPos);
 
-        var hitObject = Physics2D.Linecast(mousePos, playerPos, mask);
-
-        if (hitObject.collider != null)
+        if (hitObjects.collider != null && hitObjects.collider.tag == "Int_Molecule")
         {
-            hitObject.collider.GetComponent<SpriteRenderer>();
-        }
-        
-        //iterate enumerable 
-        foreach (var spriteRenderer in hitObjects)
-        {
-            //use the iteration of the enumerable to check and update the list
-            if (!collidingObjects.Contains(spriteRenderer))
-            {
-                collidingObjects.Add(spriteRenderer);
-            }
-        }
+            Debug.Log("hitObjects is colliding");
+            hitObjects.collider.GetComponent<SpriteRenderer>().sprite = newSprite;
+            collidingObjects.Add(hitObjects.collider.GetComponent<SpriteRenderer>());
 
-        var notLongetHitObjects = collidingObjects.Except(hitObjects).ToList();
-        //"for" needs [i]
-        //"for" iterates a specific number of times
-        //everytime it loops add one to check all elements
-        for (var i = 0; i < notLongetHitObjects.Count; i++)
-        {
-            notLongetHitObjects[i].sprite = originalSprite;
-            collidingObjects.Remove(notLongetHitObjects[i]);
         }
-
-        if (collidingObjects.Count == 0)
+        else if (collidingObjects.Count >= 1 && hitObjects.collider.tag == "Player")
         {
-            return;
+            Debug.Log("hitObjects is not colliding");
+            //linq method
+            collidingObjects.Last().GetComponent<SpriteRenderer>().sprite = originalSprite;
+            collidingObjects.RemoveAt(collidingObjects.Count - 1);
         }
+        //else if (collidingObjects.Count > 1)
+        //{
+        //    collidingObjects.Last().GetComponent<SpriteRenderer>().sprite = originalSprite;
+        //    collidingObjects.RemoveAt(collidingObjects.Count - 1);
+        //}
 
-        var closestObject = collidingObjects.OrderBy(_ => (_.transform.position - (Vector3)mousePos).sqrMagnitude).First();
-        closestObject.sprite = newSprite;
 
-        foreach (var spriteRenderer in collidingObjects.Where(_ => _ != closestObject))
-        {
-            spriteRenderer.sprite = originalSprite;
-        }
     }
 }
